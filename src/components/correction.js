6ea77193ed3abfec0fc026e502a59916e7b73749
Michel <13646125+michelBBC@users.noreply.github.com>
2019-04-27 17:33:33 +0100

    Remove references for scrolling as handled by different lib now.

--- src/components/correction.js
+++ src/components/correction.js
@@ -3,34 +3,26 @@ import shortid from 'shortid';
 
 function Correction(props, ref) {
     const [name, setName] = useState('name'); 
-    const myRef = useRef();
-
-    // To bubble up the ref to the parent component
-    useImperativeHandle(ref, () => ({
-        focus: () => {
-          myRef.current.focus();
-        }
-      }));
 
     function handleClick(e) {
         setName(e.target.key);
     }
-    console.log(myRef);
+
+    const suggestions = ['tata', 'baba', 'rara'];
+    
 
     if (!props.suggestions || !props.displaySuggestions) {
-        return <div ref={myRef} />;
+        return null;
     } else {
         return (
         <>
-            <h3 ref={myRef}>Did you mean?</h3>
-            <ul>
-            {props.suggestions.map(item => (
-                <li key={shortid.generate()} onClick={handleClick}>{item}</li>
+            <div className="corrections" ><b>Did you mean?</b>
+            {suggestions.map(item => (
+                <div key={shortid.generate()} onClick={handleClick}>{item}</div>
             ))}
-            </ul>
+            </div>
         </>
         );
     }
   }
-Correction = forwardRef(Correction)
 export default Correction;

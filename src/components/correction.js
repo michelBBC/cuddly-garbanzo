4d165d7b28ada715e2782cf4d306775573172101
Michel <13646125+michelBBC@users.noreply.github.com>
2019-04-28 23:23:10 +0100

    Fixed suggestion box bug.

--- src/components/correction.js
+++ src/components/correction.js
@@ -4,23 +4,27 @@ import './correction.css';
 import checkmark from './checkmark.svg';
 
 function Correction(props) {
-    if (props.correctSpelling || props.suggestions === null) {
-        return (
-            <>
-                <div className="corrections" ><b> {props.searchTerm} is spelled correctly</b><img src={checkmark} className="img-check" alt="check!" /></div>
-            </>
-            )
+    if (props.searchTerm) {
+        if (props.correctSpelling || props.suggestions === null) {
+            return (
+                <>
+                    <div className="corrections" ><b> {props.searchTerm} is spelled correctly</b><img src={checkmark} className="img-check" alt="check!" /></div>
+                </>
+                )
 
+        } else {
+            return (
+            <>
+                <div className="corrections" ><b>Did you mean?</b>
+                {props.suggestions.map(item => (
+                    <button key={shortid.generate()} className="text-button" value={item} onClick={props.handleSuggestionClick}><u><i>{item}</i></u></button>
+                ))}
+                </div>
+            </>
+            );
+        }
     } else {
-        return (
-        <>
-            <div className="corrections" ><b>Did you mean?</b>
-            {props.suggestions.map(item => (
-                <button key={shortid.generate()} className="text-button" value={item} onClick={props.handleSuggestionClick}><u><i>{item}</i></u></button>
-            ))}
-            </div>
-        </>
-        );
+        return null;
     }
   }
 export default Correction;
